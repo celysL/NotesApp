23e9f17ccc8893bc4d6b3ed285619c6a7b9067a8
Parminder Kaur <parminderkaur723@gmail.com>
2023-11-09 05:04:45 -0500

    updated welcome v-1

--- app/src/main/java/com/example/notesapp/Welcome.java
+++ app/src/main/java/com/example/notesapp/Welcome.java
@@ -2,7 +2,9 @@ package com.example.notesapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class Welcome extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class Welcome extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                 startActivity(new Intent(Welcome.this,MainActivity.class));
+            }
+        },  1000);
     }
 }
